1234853f3b33a59fbf7770fa3e8f521bc5256199
Rexxis <David.harendza@gmail.com>
2022-11-10 11:34:45 +0000

    feat: added check username from database and echo correct message

--- number_guess.sh
+++ number_guess.sh
@@ -5,15 +5,26 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 STEPS=0
 SECRET_NUMBER=$(echo $(( $RANDOM % 1000 + 1 )))
 
-# prompt user enter username
-
+# start prompt
+echo -e "\nEnter your username:"
+read USERNAME
 # query get username data (#username, games_played, best_game>
+USERNAME_DATA=$($PSQL "SELECT username, games_played, best_game FROM userdata WHERE username = '$USERNAME'")
 # if username not exist
+if [[ -z $USERNAME_DATA ]]
+then
   # welcome new user msg
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
   # function main game
+else
 # if username exist
   # welcome existing user message 
+  echo "$USERNAME_DATA" | while IFS="|" read USERNAME GAMES_PLAYED BEST_GAME
+  do
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   # function main game
+  done
+fi
 
 # - MAIN GAME FUNCTION -
 # echo Guess the secret number.......
